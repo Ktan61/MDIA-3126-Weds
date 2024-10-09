91ec1e3de0e175c62b8527e4e96b24e9a58afeb5
Kathryn Tan <144634477+Ktan61@users.noreply.github.com>
2024-10-08 23:31:26 -0700

    Tailwind / NativeWind installation from Lesson 4

--- mobile-app/app/(tabs)/cheese.tsx
+++ mobile-app/app/(tabs)/cheese.tsx
@@ -1,11 +1,12 @@
-import { Text, View } from 'react-native';
+import { Text, View , StyleSheet, Button } from 'react-native';
 import { Link } from 'expo-router';
 import { Pressable } from 'react-native';
 
 export default function Page() {
   return (
-    <View>
-        <Text>This is a page about cheese.</Text>
+    <View style={makeItPretty.testing} className='m-2'>
+        <Button title="BUTTON KUDASAI"></Button>
+        <Text className='border-red-700 border-2 '>This is a page about cheese.</Text>
         <Link href="/" asChild>
             <Pressable>
                 <Text>Index 🍓🍓</Text>
@@ -14,3 +15,12 @@ export default function Page() {
     </View>
   )
 }
+
+const makeItPretty = StyleSheet.create({
+  testing: {
+    marginTop: 50,
+    borderColor: 'blue',
+    borderWidth: 3,
+    borderStyle: 'dashed'
+  }
+})
